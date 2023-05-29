5f200223eee29b7a663f0f677208417d2600a808
georginafolco <folcogeorgina@gmail.com>
2023-05-29 20:50:52 -0300

    preparacion template para el parcial

---
+++ primer-parcial-front3/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+
+  return (
+    <div>
+      <h1>Carga de estudiantes</h1>
+      <form></form>
+      <Card />
+    </div>
+  )
+}
+
+export default App
