21b818d0383b89a0d1cb7114d2e87a83587f49c1
georginafolco <folcogeorgina@gmail.com>
2023-05-31 20:16:45 -0300

    primer parcial terminado

--- primer-parcial-front3/src/App.jsx
+++ primer-parcial-front3/src/App.jsx
@@ -1,14 +1,41 @@
+import { useState } from 'react'
 import './App.css'
-import Card from './components/Card'
+import Card from './components/Tarjeta/Card'
+import Form from './components/Formulario/Form'
 
 function App() {
 
+  const [inscripto, setInscripto] = useState({
+    nombre: '',
+    materia: ''
+  });
+
+  const [state, setState] = useState({
+    success: false, 
+    fail: false
+  })
+
+  const handleSubmit = (e) => {
+    console.log("handleSubmit")
+    e.preventDefault()
+
+
+    let nombreInscripto = inscripto.nombre.trim();
+
+    if (nombreInscripto.length > 3 && inscripto.materia.length > 6){    
+      setState({success: true, fail: false})
+    } else {
+      setState({success: false, fail: true})
+    }
+  }
+    
   return (
-    <div>
-      <h1>Carga de estudiantes</h1>
-      <form></form>
-      <Card />
-    </div>
+    <>
+    <h2 className="title">Inscripción a Materias</h2>
+      <Form setInscripto={setInscripto} handleSubmit={handleSubmit} state={state}/>
+      {state.success && <Card inscripto={inscripto} />}
+      {state.fail && <p className='errorMsg'>Por favor chequea que los datos ingresados sean correctos.</p>}
+    </>
   )
 }
 
